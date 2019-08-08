d76906b8d8efe721df81d41c481cb8a2c3229e3d
lizongsheng <576500670@qq.com>
2019-08-08 14:48:32 +0800

    f

--- business-model/src/test/java/top/toptimus/meta/BuildMetaTest.java
+++ business-model/src/test/java/top/toptimus/meta/BuildMetaTest.java
@@ -6,17 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import top.toptimus.BusinessModelApplication;
-import top.toptimus.controller.MetaController;
-import top.toptimus.formula.FormulaKeyAndValueDTO;
 import top.toptimus.meta.metaview.MetaInfoDTO;
+import top.toptimus.service.domainService.MetaService;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = BusinessModelApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -25,41 +21,44 @@ public class BuildMetaTest {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private MetaController metaController;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private MetaService metaService;
+
 
     @Test
     public  void createMasterData() {
 
         //输入metaId
-         String metaId = "chunshan_dingdan_bill";
-        String metaName = "订单信息";
+        String metaId = "chunshan_mianliao_entry";
+        String metaName = "面料配套信息";
 
 
         System.out.println("----------------创建MetaInfo----------------");
+
         //保存meta信息
-        List<MetaInfoDTO> metaInfoDTOList = this.buildMeta(metaId,metaName);
-        metaController.save(metaInfoDTOList);
-        metaController.createTable(metaId,new ArrayList<String>(){{
-            add(metaId);
-        }});
+        List<MetaInfoDTO> metaInfoDTOList = this.buildMeta(metaId, metaName);
+        metaService.saveMetaInfoDTO(metaInfoDTOList);
     }
 
     /**
      * metainfomation
+     *
      * @return
      */
-    private List<MetaInfoDTO> buildMeta(String metaId,String metaName){
-        List<MetaInfoDTO> metaInfoDTOList = new ArrayList<MetaInfoDTO>(){{
-            add(new MetaInfoDTO(metaId,metaName,"dingdanriqi","订单日期","DATE",true,false,true,"","","","","1","DATE"));
-            add(new MetaInfoDTO(metaId,metaName,"dianpu","店铺","STRING",true,false,true,"","","","","1","STRING"));
-            add(new MetaInfoDTO(metaId,metaName,"zhuozhuangguwen","着装顾问","STRING",true,false,true,"","","","","1","STRING"));
-            add(new MetaInfoDTO(metaId,metaName,"kehuxingming","客户姓名","STRING",true,false,true,"","","","","1","STRING"));
-            add(new MetaInfoDTO(metaId,metaName,"kehudizhi","客户地址","STRING",true,false,true,"","","","","1","STRING"));
-            add(new MetaInfoDTO(metaId,metaName,"dianhuahaoma","电话号码","DECIMAL",true,false,true,"","","","","1","DECIMAL"));
-            add(new MetaInfoDTO(metaId,metaName,"weixin","微信","STRING",true,false,true,"","","","","1","STRING"));
+    private List<MetaInfoDTO> buildMeta(String metaId, String metaName) {
+        return new ArrayList<MetaInfoDTO>() {{
+            add(new MetaInfoDTO(metaId, metaName, "mianliaomingcheng", "面料名称", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "pipeikuanshi", "匹配款式", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "gongyingshang", "供应商", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaozhifa", "面料织法", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "miaoliaofengge", "面料风格", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaochengfen", "面料成份", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaofukuan", "面料幅宽", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaodanjia", "面料单价", "DECIMAL", true, false, true, "", "", "", "", "1", "CURRENCY"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaoyunfei", "面料运费", "DECIMAL", true, false, true, "", "", "", "", "1", "CURRENCY"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaohoudu", "面料厚度", "DECIMAL", true, false, true, "", "", "", "", "1", "DECIMAL"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaojijie", "面料季节", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaosuolv", "面料缩率", "DECIMAL", true, false, true, "", "", "", "", "1", "DECIMAL"));
+            add(new MetaInfoDTO(metaId, metaName, "mianliaozhangli", "面料张力", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
         }};
-        return metaInfoDTOList;
     }
 }
