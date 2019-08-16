f5a78eabd197c58c11f76841114e64085818a784
gaoyu <gaofangchoushabi>
2019-08-16 11:12:13 +0800

    批量导入

--- business-model/src/test/java/top/toptimus/meta/BuildMetaTest.java
+++ business-model/src/test/java/top/toptimus/meta/BuildMetaTest.java
@@ -9,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import top.toptimus.BusinessModelApplication;
+import top.toptimus.entity.meta.event.MetaEventEntity;
+import top.toptimus.entity.meta.event.MetaGenerateEntity;
 import top.toptimus.meta.csv.MetaCSVImportModel;
 import top.toptimus.meta.metaview.MetaInfoDTO;
-import top.toptimus.service.domainService.MetaService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,29 +24,41 @@ public class BuildMetaTest {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private MetaService metaService;
+    private MetaEventEntity metaEventEntity;
+
+    @Autowired
+    private MetaGenerateEntity metaGenerateEntity;
 
     @Test
-    public void createMasterData() {
-
-        //输入metaId
-        String metaId = "chunshan_dingdan_bill";
-        String metaName = "订单信息";
-        logger.info(
-                JSON.toJSONString(this.buildMeta(metaId, metaName, "/Users/gaoyu/Desktop/测试.csv"))
+    public void testImportBaseMeta(){
+        metaGenerateEntity.importBaseMeta(
+                "/Users/gaoyu/Desktop/testData/metaIndex.csv"
+                ,"/Users/gaoyu/Desktop/testData"
         );
-
-        System.out.println("----------------创建MetaInfo----------------");
-
-        //保存meta信息
-        List<MetaInfoDTO> metaInfoDTOList = this.buildMeta(metaId, metaName);
-        metaService.saveMetaInfoDTO(metaInfoDTOList);
     }
+
+//    @Test
+//    public void createMasterData() {
+//
+//        //输入metaId
+//        String metaId = "chunshan_dingdan_bill";
+//        String metaName = "订单信息";
+//        logger.info(
+//                JSON.toJSONString(MetaCSVImportModel.generateMetaInfoFromCSV(metaId, metaName, "/Users/gaoyu/Desktop/测试.csv"))
+//        );
+//
+//        System.out.println("----------------创建MetaInfo----------------");
+//
+//        //保存meta信息
+//        metaEventEntity.saveMetaInfoDTO(
+//                MetaCSVImportModel.generateMetaInfoFromCSV(metaId, metaName, "/Users/gaoyu/Desktop/测试.csv")
+//        );
+//    }
 
     /**
      * metainfomation
      *
-     * @return
+     * @return meta info列表
      */
     private List<MetaInfoDTO> buildMeta(String metaId, String metaName) {
         return new ArrayList<MetaInfoDTO>() {{
@@ -63,9 +76,5 @@ public class BuildMetaTest {
             add(new MetaInfoDTO(metaId, metaName, "mianliaosuolv", "面料缩率", "DECIMAL", true, false, true, "", "", "", "", "1", "DECIMAL"));
             add(new MetaInfoDTO(metaId, metaName, "mianliaozhangli", "面料张力", "STRING", true, false, true, "", "", "", "", "1", "STRING"));
         }};
-    }
-
-    private List<MetaInfoDTO> buildMeta(String metaId, String tokenMetaName, String filePath) {
-        return MetaCSVImportModel.generateMetaInfoFromCSV(metaId, tokenMetaName, filePath);
     }
 }
--- dependency-package/dto-model/src/main/java/top/toptimus/meta/csv/MetaCSVImportModel.java
+++ dependency-package/dto-model/src/main/java/top/toptimus/meta/csv/MetaCSVImportModel.java
@@ -26,14 +26,24 @@ public class MetaCSVImportModel {
     public static List<MetaInfoDTO> generateMetaInfoFromCSV(String metaId, String tokenMetaName, String filePath) {
         List<String> dataList = CSVUtils.importCsv(filePath);
         return new ArrayList<MetaInfoDTO>() {{
-            if (dataList != null && !dataList.isEmpty()) {
+            if (!dataList.isEmpty()) {
                 for (int i = 0; i < dataList.size(); i++) {
                     if (i != 0) {//不读取第一行
                         String s = dataList.get(i);
                         String[] as = s.split(",");
                         if (as[2].equals(FkeyTypeEnum.SELECT.name())) {
                             //  SELECT类型
-                            add(new MetaInfoDTO(metaId, tokenMetaName, as[0], as[1], as[2], as[3], as[4]));
+                            add(
+                                    new MetaInfoDTO(
+                                            metaId
+                                            , tokenMetaName
+                                            , as[0]
+                                            , as[1]
+                                            , as[2]
+                                            , as[3]
+                                            , "id"  //  TODO    硬编码
+                                    )
+                            );
                         } else {
                             //  普通类型
                             add(new MetaInfoDTO(metaId, tokenMetaName, as[0], as[1], as[2]));
@@ -42,6 +52,26 @@ public class MetaCSVImportModel {
                 }
             }
         }};
+    }
 
+    /**
+     * 读取索引文件
+     *
+     * @param indexFilePath 索引文件路径
+     * @return meta索引文件列表
+     */
+    public static List<MetaIndexDTO> generateMetaIndexDTO(String indexFilePath) {
+        List<String> dataList = CSVUtils.importCsv(indexFilePath);
+        return new ArrayList<MetaIndexDTO>() {{
+            if (!dataList.isEmpty()) {
+                for (int i = 0; i < dataList.size(); i++) {
+                    if (i != 0) {//不读取第一行
+                        String s = dataList.get(i);
+                        String[] as = s.split(",");
+                        add(new MetaIndexDTO(as[0], as[1]));
+                    }
+                }
+            }
+        }};
     }
 }
